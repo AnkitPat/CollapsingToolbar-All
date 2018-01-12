0d7c4283fba919d424ab4e5e68d650022c53ba45
Ankit Patidar <jaykishor.dwivedi@media4u.com>
2018-01-12 19:08:29 +0530

    Commit after some view

--- App.js
+++ App.js
@@ -4,10 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Platform, StatusBar, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StatusBar, Text, View,StyleSheet,Image} from 'react-native';
 import CollapsibleToolbar from 'react-native-collapsible-toolbar';
 import NavBackButton from './NavBackButton';
+
+import {Card} from 'react-native-elements'
+import Button from "./Button";
+
 
 export default class App extends Component {
     componentWillMount() {
@@ -21,20 +25,36 @@ export default class App extends Component {
 
     renderContent = () => (
         <View>
-            {new Array(20).fill().map((_, i) => (
-                <View
-                    // eslint-disable-next-line
-                    key={i}
-                    style={{
-                        backgroundColor: '#F5F5F5',
-                        padding: 10,
-                        borderBottomWidth: 1,
-                        borderBottomColor: '#E5E5E5'
-                    }}
-                >
-                    <Text>{`Item ${i + 1}`}</Text>
-                </View>
-            ))}
+
+            <View>
+                <Card style={{height: 200, backgroundColor: '#f00'}}>
+
+
+                    <Text style={styles.headerText}>Hello ALl</Text>
+
+                    <View style={{marginTop:20, flex: 1,flexDirection: 'row'}}>
+                        <Image source={require('./assets/location.png')} style={{alignSelf:'center',flex:0.1,width:40,height:40}}/>
+                        <View style={{flexDirection:'column',flex:0.9}}>
+                            <Text style={styles.locationText}>Laxmi Lawns: Pune</Text>
+                            <Text style={styles.fullLocationText}>Next to Magarpattacity, 133, Magarpatta,....</Text>
+                        </View>
+                    </View>
+
+                    <View style={{marginTop:20, flex: 1,flexDirection: 'row'}}>
+                        <Image source={require('./assets/calendar.png')} style={{alignSelf:'center',flex:0.1,width:40,height:40}}/>
+                        <View style={{flexDirection:'column',flex:0.6}}>
+                            <Text style={styles.dateText}>Laxmi Lawns: Pune</Text>
+                            <Text style={styles.priceText}>Next to Magarpattacity, 133, Magarpatta,....</Text>
+                        </View>
+                        <View style={{flex:0.3, justifyContent:'center'}}>
+                            <Button title={'BOOK'}/>
+                        </View>
+
+                    </View>
+
+                </Card>
+            </View>
+
         </View>
     );
 
@@ -47,10 +67,10 @@ export default class App extends Component {
                 flex: 1
             }}
         >
-            <NavBackButton title='' onPress={()=>{
+            <NavBackButton title='' onPress={() => {
                 console.log('ankit back')
-            }}  />
-            <Text style={{ textAlign: 'center', color: '#FFF' }}>Title</Text>
+            }}/>
+            <Text style={{textAlign: 'center', color: '#FFF'}}>Title</Text>
         </View>
     );
 
@@ -63,8 +83,44 @@ export default class App extends Component {
                 collapsedNavBarBackgroundColor='#009688'
                 translucentStatusBar
                 showsVerticalScrollIndicator={false}
-                // toolBarHeight={300}
+                toolBarHeight={150}
             />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    headerText: {
+        fontWeight:'bold',
+        fontSize: 20,
+        color: '#000'
+    },
+
+    dateText: {
+        color:'#777777',
+        marginLeft: 20,
+        fontSize: 14,
+        marginTop: 5
+    },
+
+    priceText: {
+        color:'#3E3E3E',
+      fontSize: 14,
+      marginTop: 7,
+      marginLeft: 20
+    },
+
+    fullLocationText: {
+        color:'#777777',
+        fontSize: 16,
+        marginLeft: 20,
+        marginTop: 7
+    },
+    locationText: {
+        color:'#777777',
+        marginLeft: 20,
+        fontSize: 18,
+        color: '#0E84E0',
+        marginTop: 5
+    }
+});
---
+++ Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    Text, TouchableOpacity,ToastAndroid
+} from 'react-native'
+
+import styles from './style/ButtonStyleSimple'
+
+const Button = ({title,onPress})=> {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.buttonStyle}>
+
+            <Text style={styles.textStyle}>{title}</Text>
+
+        </TouchableOpacity>
+    );
+};
+export  default Button;
